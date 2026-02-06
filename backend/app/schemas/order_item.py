bc824897ca4ffe5d6cf258d12c186e61e822aed2
qnderkk <Qndi@yandex.ru>
2026-02-06 04:17:19 +0300

    done schemas

---
+++ backend/app/schemas/order_item.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, Field
+from typing import List
+from datetime import datetime
+
+
+class OrderItemBase(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., gt=0, description="Quantity")
+
+
+class OrderItemCreaete(OrderItemBase):
+    pass
+
+
+class OrderItemUpdate(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., gt=0, description="Quantity")
+
+
+class OrderItemResponse(BaseModel):
+    product_id: int = Field(..., description="Product ID")
+    quantity: int = Field(..., gt=0, description="Quantity")
+    price: int = Field(..., description="Product price")
+
+    class Config:
+        from_attributes = True
+
+
+class OrderBase(BaseModel):
+    delivery_address: str = Field(..., min_length=1, description="Order address")
+    items: List[OrderItemCreaete]
+
+
+class OrderCreate(OrderBase):
+    pass
+
+
+class OrderResponse(BaseModel):
+    id: int
+    user_id: int
+    status: str
+    total_amount: float
+    delivery_address: str
+    created_at: datetime
+    
+    items: List[OrderItemResponse]
+
+    class Config:
+        from_attributes = True
